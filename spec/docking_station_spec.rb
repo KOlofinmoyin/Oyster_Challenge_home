58e5cf9af11f650091e1002ca13aa4afa11643be
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-12-23 21:30:51 +0000

    User Story (delivered): As a member of the public So I can decide whether to use the docking station I want to see a bike that has been docked

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,11 +13,11 @@ describe DockingStation do
     expect(stratford_docking_station.release_bike).to be_a(Bike)
   end
 
-  # As a member of the public
-  # So I can return bikes I've hired
-  # I want to dock my bike at the docking station
-
   it "docks a bike at the docking station" do
     expect(stratford_docking_station.dock(bike)).to eq bike
+  end
+
+  it "displays a bike that has been docked" do
+
   end
 end
